82a70561bd468b274200a57d4f14a06ec3af962f
xudChen <xudong.chen@jumacc.com>
2014-12-25 16:27:19 +0800

    init project

---
+++ js/juma.js
@@ -0,0 +1,97 @@
+var juma = {
+
+    baseUrl : "http://127.0.0.1:8086/ble",
+    device : null, 
+    
+    emulation_mode : function(value){
+        if(!value){
+        	console.log('value is not be null');
+        	return;
+		}
+        if(value == 0){
+            this.baseUrl = "http://127.0.0.1:8086/ble";
+        }else{
+            this.baseUrl = "http://api.juma.io/ble";
+        }     
+    },
+
+    status:function(){
+        var tempUrl = this.baseUrl +'/status';
+        $.getJSON(tempUrl,null,function(data){
+            alert(JSON.stringify(data));
+        });
+    },
+    
+    scan:function(selector,settings){
+        if(!selector){
+        	console.log('no selector');
+        	return;
+        }
+        var tempUrl = this.baseUrl +'/devices/nearby';
+        
+        $(selector).html("searching");
+        
+        $.getJSON(tempUrl,null,function(data){
+            var devices = data['devices'];
+
+            if (devices.length == 0) {
+                $(selector).html('no device found');
+                return;
+            }
+
+            html_str = "";
+            for(var i=0;i<devices.length;i++){
+                var device = devices[i];
+                var name = device.name;
+                var addr = device.address;
+                var rssi = device.rssi;
+
+                html_str += '<label for="device-' + i + '">' + name + ' &nbsp;&nbsp' + addr;
+                html_str += ' ' + '</label><input type="radio" name="device" id="device-' + i;
+                html_str += '" value="' + i + '"></input>';
+            }
+
+            $(selector).html(html_str);
+
+            $('input[type="radio"]').checkboxradio();
+
+            for(var i=0;i<devices.length;i++) {
+                (function (device) {
+                    $('#device-' + i).click(function() {
+                        juma.device = device;
+                    });
+                })(devices[i]);
+            }
+          });
+    },
+    
+    read : function(resource,success){
+    	if(!this.device){
+	    	console.log('please select device at first!');
+	    	return;
+	    }
+        if(!resource){
+            console.log('resource is not be null!');
+            return;
+        }
+        var tempUrl = this.baseUrl+'/device/'+this.device.deviceAddress+resource;
+        $.getJSON(tempUrl, null, function(data){
+            if(success)success(data['value']);
+        });
+    },
+    
+    write : function(resource,params){
+	    if(!this.device){
+	    	console.log('please select device at first!');
+	    	return;
+	    }
+		if(!resource){
+			console.log('resource is not be null!');
+			return;
+		}
+        var tempUrl = this.baseUrl+'/device/'+this.device.deviceAddress+resource;
+        $.post(tempUrl, params, function(data){
+            console.log(JSON.stringify(data));
+        });
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+	$('#mode').change(function(){
+		juma.emulation_mode($(this).val());
+	});
+
+	$('#scan-button').click(function() {
+		juma.scan('#device-list');
+    });
+
+	$('#temperature').click(function() {
+	    juma.read("/service/temperature",function(value){
+		    $('#temperature-value').html(value+"℃");
+	    });
+	});
+
+	$('#beep-button').click(function() {
+	    juma.write("/service/buzzer",{'duration':'5s'});
+	}); 
+	
+	var canvas = document.getElementById('picker');
+	var ctx = canvas.getContext('2d');
+	var image = new Image();
+	image.onload = function(){
+	    ctx.drawImage(image, 0, 0, image.width, image.height);
+	}
+	var imageSrc = 'img/colorwheel1.png';
+	image.src = imageSrc;
+	$('#picker').click(function(e) {
+		var canvasOffset = $(canvas).offset();
+        var canvasX = Math.floor(e.pageX - canvasOffset.left);
+        var canvasY = Math.floor(e.pageY - canvasOffset.top);
+        var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+        var pixel = imageData.data;
+        var pixelColor = "rgb("+pixel[0]+", "+pixel[1]+", "+pixel[2]+")";
+        var dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
+		$('#hexVal').css('color',' #' + ('0000' + dColor.toString(16)).substr(-6));
+        $('#hexVal').html(' #' + ('0000' + dColor.toString(16)).substr(-6)+' ');
+		juma.write("/service/light",{'color':('0000' + dColor.toString(16)).substr(-6)});
+	}); 
+});
